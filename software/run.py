26379f50354978139a322bbda0120327c45800c5
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2023-11-27 18:29:57 -0800

    added confusion matrix

--- software/run.py
+++ software/run.py
@@ -2,8 +2,10 @@ import numpy as np
 import torch
 import torch.nn as nn
 import argparse
+import matplotlib.pyplot as plt
 from util import load_data_n_model
 from scipy import signal
+from collections import defaultdict
 
 def train(model, tensor_loader, num_epochs, learning_rate, criterion, device):
     model = model.to(device)
@@ -39,6 +41,10 @@ def test(model, tensor_loader, criterion, device):
     model.eval()
     test_acc = 0
     test_loss = 0
+
+    # create an accuracy map, later used for the confusion matrix
+    accuracy_map = defaultdict(list)
+
     for data in tensor_loader:
         inputs, labels = data
         inputs = inputs.to(device)
@@ -48,16 +54,30 @@ def test(model, tensor_loader, criterion, device):
         outputs = model(inputs)
         outputs = outputs.type(torch.FloatTensor)
         outputs.to(device)
+
+        for i,j in zip(labels,outputs):
+            nt = torch.zeros_like(j)
+            nt[torch.argmax(j)] = 1
+            accuracy_map[i.item()].append(nt)
         
         loss = criterion(outputs,labels)
         predict_y = torch.argmax(outputs,dim=1).to(device)
         accuracy = (predict_y == labels.to(device)).sum().item() / labels.size(0)
         test_acc += accuracy
         test_loss += loss.item() * inputs.size(0)
+
+    confusion_matrix = np.zeros((len(accuracy_map), len(accuracy_map)))
+    for k in accuracy_map:
+        accuracy_map[k] = torch.stack(accuracy_map[k])
+        accuracy_map[k] = torch.sum(accuracy_map[k], dim=0)
+        accuracy_map[k] = accuracy_map[k].cpu().numpy()
+        accuracy_map[k] = accuracy_map[k] / np.sum(accuracy_map[k])
+        confusion_matrix[k] = accuracy_map[k]
+
     test_acc = test_acc/len(tensor_loader)
     test_loss = test_loss/len(tensor_loader.dataset)
     print("validation accuracy:{:.4f}, loss:{:.5f}".format(float(test_acc),float(test_loss)))
-    return
+    return confusion_matrix
 
     
 def main():
@@ -80,12 +100,40 @@ def main():
         criterion=criterion,
         device=device
          )
-    test(
+    cm = test(
         model=model,
         tensor_loader=test_loader,
         criterion=criterion,
         device= device
         )
+
+    # plot the confusion matrix
+    fig, ax = plt.subplots()
+    plt.figure(figsize=(6,6))
+
+    # does not work with UTHAR
+
+    # TODO: get the appropriate class names
+    if args.dataset == 'NTU-Fi_HAR':
+        category_map = test_loader.dataset.category
+        class_names = [category_map[i] for i in range(len(category_map))]
+    else:
+        class_name = ['1','2','3','4','5','6']
+
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title('Epoch = {}'.format(train_epoch), pad=20)
+    plt.colorbar()
+
+    # TODO: this will probably cause errors for other data sets
+    tick_marks = np.arange(6)
+    plt.xticks(tick_marks, class_names)
+    plt.yticks(tick_marks, class_names, va='center')
+    plt.ylabel('Actual label')
+    plt.xlabel('Predicted label')
+
+    # TODO: make the name more specific
+    plt.savefig('results/confusion_matrix.pdf')
+
     return
 
 
