ad3e2301bc46d51723ebfd6e4a585539057b5eeb
emagana6622 <123432248+emagana6622@users.noreply.github.com>
2023-12-15 01:11:08 -0800

    Update dataset.py

--- software/dataset.py
+++ software/dataset.py
@@ -3,7 +3,7 @@ import glob
 import scipy.io as sio
 import torch
 from torch.utils.data import Dataset, DataLoader
-
+from scipy.signal import decimate 
 
 
 def UT_HAR_dataset(root_dir):
@@ -14,7 +14,7 @@ def UT_HAR_dataset(root_dir):
         data_name = data_dir.split('/')[-1].split('.')[0]
         with open(data_dir, 'rb') as f:
             data = np.load(f)
-            data = data[:,::50] #edit to downsample 
+            data = data[:,::50]
             data = data.reshape(len(data),1,5,90) #edit the second to last digit to match your new matrix size
             data_norm = (data - np.min(data)) / (np.max(data) - np.min(data))
         WiFi_data[data_name] = torch.Tensor(data_norm)
@@ -60,14 +60,17 @@ class CSI_Dataset(Dataset):
         x = (x - 42.3199)/4.9802
         
         # sampling: 2000 -> 500
-        x = x[:,::2] #edit to downsample
-        x = x.reshape(3, 114, 1000) #edit the last digit to match your new matrix
-       
+        x = x[:,::1000] #edit to downsample
+        #x = x[::2,:]
+        #x = decimate(x, q=8, zero_phase=True)
+        #x=x.copy()
+        x = x.reshape(3, 114, 2) #edit the last digit to match your new matrix
+        #x=x[:,0:37,:] #used to select subcarriers
         if self.transform:
             x = self.transform(x)
         
         x = torch.FloatTensor(x)
-        
+        #print(x.size())
         return x,y
 
 
@@ -93,10 +96,30 @@ class Widar_Dataset(Dataset):
         x = (x - 0.0025)/0.0119
         
         # reshape: 22,400 -> 22,20,20
+        #x = decimate(x, q=8, zero_phase=True)
+        
+        #x=x.copy()
+        #x = x[::4,::4]
         x = x.reshape(22,20,20)
+        x = self.downsample(x, 1)
+        
         # interpolate from 20x20 to 32x32
         # x = self.reshape(x)
+        
         x = torch.FloatTensor(x)
-
+        #print(x.size())
         return x,y
+    def downsample(self, x, factor):
+        """
+        Downsamples a 3D data tensor by a given factor.
+
+        Args:
+        x: The data tensor of shape (time, height, width).
+        factor: The downsampling factor.
+
+        Returns:
+        The downsampled data tensor.
+        """
+        x_downsampled = x[:, ::factor, ::factor]
+        return x_downsampled
 
